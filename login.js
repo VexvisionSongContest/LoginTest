c215a12c1e4bb39298773c027f58ddad68f4bd87
VVSC <fab_limberger@icloud.com>
2026-06-26 08:38:32 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 // Paste your credentials here
-const supabase = supabase.createClient('YOUR_PROJECT_URL', 'YOUR_ANON_KEY');
+const supabase = supabase.createClient('https://xjxwniomwrtcisayoeyj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqeHduaW9td3J0Y2lzYXlvZXlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0Mjc2NDMsImV4cCI6MjA5ODAwMzY0M30.6K-ViMwz2--e9TZ8QnX6xsXk8_r_4gbUtxEyxFgvIA0');
 
 async function signUp() {
     const { error } = await supabase.auth.signUp({
